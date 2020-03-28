d1f59a2ed2ee7c1ffd3f98c85791485142b28f9a
Jono <jjharrington@gmail.com>
2020-03-28 16:18:00 +1300

    FIX cm not mm stupid

--- Design/Final/Code/utils.py
+++ Design/Final/Code/utils.py
@@ -7,9 +7,9 @@ jh, Mar 2020
 
 def height2ibw(gender, height_cm):
     if gender=='f':
-        return 45.5 + 2.3*(height_cm - 5*12*25.4)/2.54
+        return 45.5 + 2.3*(height_cm - 5*12*2.54)/2.54
     elif gender=='m':
-        return 50.0 + 2.3*(height_cm - 5*12*25.4)/2.54
+        return 50.0 + 2.3*(height_cm - 5*12*2.54)/2.54
 
 
 
