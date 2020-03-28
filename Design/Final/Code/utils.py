64b6dd61303f5155e8a17931417c0c34ca417ed9
Jono <jjharrington@gmail.com>
2020-03-28 16:22:06 +1300

    ENH tidal volume from Ideal Body Weight

--- Design/Final/Code/utils.py
+++ Design/Final/Code/utils.py
@@ -6,12 +6,22 @@ jh, Mar 2020
 
 
 def height2ibw(gender, height_cm):
+    """Compute Ideal Body Weight from patient height.
+
+    https://globalrph.com/medcalcs/adjusted-body-weight-ajbw-and-ideal-body-weight-ibw-calc/
+    """
     if gender=='f':
         return 45.5 + 2.3*(height_cm - 5*12*2.54)/2.54
     elif gender=='m':
         return 50.0 + 2.3*(height_cm - 5*12*2.54)/2.54
 
 
-
+def ibw2tv(ibw):
+    """Tidal Volume from Ideal Body Weight
+    """
+    return 8*ibw # ml
+    
+    
 if __name__=="__main__":
-    print(height2ibw('m', 192))
+    print(ibw2tv(height2ibw('m', 192)))
+    print(ibw2tv(height2ibw('f', 192)))
