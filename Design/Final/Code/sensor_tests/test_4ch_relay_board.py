8eed94dda239aa1d3a1db9283d0e00c06d202b7d
Ra Cleave <ra.cleave@gmail.com>
2020-03-28 03:13:20 +0000

    Testing of 4 ch relay

---
+++ Design/Final/Code/sensor_tests/test_4ch_relay_board.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+fs = 1
+
+while 1:
+  led.on()
+  print(led.is_lit)
+  sleep(1/(2*fs))
+  led.off()
+  print(led.is_lit)
+  sleep(1/(2*fs))
+
